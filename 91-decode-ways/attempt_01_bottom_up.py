bec6cf06be1707cd79194a1349b06bf2903a1086
Donald Kiser <kiserd@oregonstate.edu>
2021-12-30 01:11:11 -0800

    removed a couple files from decode ways

--- 91-decode-ways/attempt_01_bottom_up.py
+++ 91-decode-ways/attempt_01_bottom_up.py
@@ -1,49 +1,28 @@
 # bottom-up
 # use int() to start, maybe create helper functions later
+# working backward per problem hint
 class Solution:
     def numDecodings(self, s: str) -> int:
-        # handle base case of leading '0'
-        if s[0] == '0':
-            return 0
         # build memo array, implicitly set base case
-        memo = [[0] * len(s) for i in range(3)]
-        memo[1][0] = 1
-        memo[2][0] = 1
-        # DEBUG PRINTING
-        print('memo:')
-        for row in memo:
-            print(row)
-        ################
+        memo = [0] * (len(s) + 1)
+        memo[len(s)] = 1
         # work up to full string
-        for i in range(1, 3, 1):
-            for j in range(1, len(s), 1):
-                print('i: ', i)
-                print('j: ', j)
-                print('s[j - i + 1: j + 1]: ', s[j - i + 1: j + 1])
-                # handle case where all three substrings valid
-                if self.is_valid(s[j - i + 1: j + 1]):
-                    memo[i][j] = memo[i - 1][j] + memo[i][j - 1]
+        for i in range(len(s) - 1, -1, -1):
+            # handle base case of leading 0
+            if s[i] == '0':
+                memo[i] = 0
+            # handle case where two-char is in bounds
+            elif i < len(s) - 1:
+                # handle case where two-char is valid
+                if self.is_valid(s[i: i + 2]):
+                    memo[i] = memo[i + 1] + memo[i + 2]
+                # handle case where two-char is invalid
                 else:
-                    memo[i][j] = memo[i - 1][j]
-                # DEBUG PRINTING
-                print('memo:')
-                for row in memo:
-                    print(row)
-                ################
-        return memo[2][len(s) - 1]
+                    memo[i] = memo[i + 1]
+            # handle (maybe base) case of last non-zero substring 
+            else:
+                memo[i] = 1
+        return memo[0]
     
     def is_valid(self, s):
-        if len(s) == 2:
-            return int(s) > 9 and int(s) < 27
-        elif len(s) == 1:
-            return int(s) > 0 and int(s) < 10
-        else:
-            print('*** DEBUG: passed weird string to is_valid()')
-
-
-
-
-
-        # for char in s:
-        #     print(f'ord({char}) = {ord(char)}')
-        #     print(f'mod 48: {ord(char) % 48}')
+        return int(s) > 9 and int(s) < 27
