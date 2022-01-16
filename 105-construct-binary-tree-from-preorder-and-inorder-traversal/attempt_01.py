dfa8ed7107f6c9e3a6673b504a42661d73ae20b5
Donald Kiser <kiserd@oregonstate.edu>
2022-01-16 07:36:37 -0800

    didn't complete 105, but followed clever solution through to where I understand very well

---
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/attempt_01.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, preorder, inorder):
+        def helper(left, right):
+            # use nonlocal to refer to next root idx
+            nonlocal idx
+            # handle base case
+            if left > right: return None
+            # construct the root node
+            root_val = preorder[idx]
+            root = TreeNode(root_val)
+            idx += 1
+            # recursively build left and right subtree
+            root_idx = num_to_idx[root_val]
+            root.left = helper(left, root_idx - 1)
+            root.right = helper(root_idx + 1, right)
+            return root
+        # create inorder num to idx mapping
+        num_to_idx = {}
+        for i, num in enumerate(inorder):
+            num_to_idx[num] = i
+        # initialize global root idx tracker
+        idx = 0
+        return helper(0, len(preorder) - 1)
+
+
+# definitely need to return to this down the road and build from memory.
+# this first attempt relied HEAVILY on the solution provided by LC.
+# going to take a hack at some of the other 'construct tree from...' problems
+    
--- 163-missing-ranges/attempt_01.py
+++ 163-missing-ranges/attempt_01.py
@@ -1,2 +1,27 @@
 class Solution:
     def findMissingRanges(self, nums, lower: int, upper: int):
+        # use two pointers
+        nums.append(upper + 1)
+        i = lower
+        j = 0
+        st = i
+        ed = i
+        res = []
+        while i <= upper:
+            # handle case where next element is not sequential
+            if nums[j] != i:
+                ed = nums[j] - 1
+                # handle case of single elt range
+                if st == ed:
+                    res.append(str(st))
+                else:
+                    res.append(str(st) + '->' + str(ed))
+                i = nums[j] + 1
+                st = i
+                j += 1
+            # handle case where next elt is sequential
+            else:
+                j += 1
+                i += 1
+                st += 1
+        return res
