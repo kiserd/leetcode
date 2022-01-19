6672faa6ea8217e2509b966851bffeaef2db15ef
Donald Kiser <kiserd@oregonstate.edu>
2022-01-18 16:15:56 -0800

    added zigzag binary tree

--- 103-binary-tree-zigzag-level-order-traversal/attempt_01.py
+++ 103-binary-tree-zigzag-level-order-traversal/attempt_01.py
@@ -4,37 +4,31 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import deque
 class Solution:
     def zigzagLevelOrder(self, root):
-        pass        
-
-
-
-
-# dumb attempt
-
-# class Solution:
-#     def zigzagLevelOrder(self, root):
-#         q = [root]
-#         next = 0
-#         res = []
-#         size = 1
-#         rev = True
-#         while next < len(q):
-#             rev = not rev
-#             curr = []
-#             for _ in range(size):
-#                 if next == len(q):
-#                     break
-#                 node = q[next]
-#                 next += 1
-#                 curr.append(node.val)
-#                 if node.left:
-#                     q.append(node.left)
-#                 if node.right:
-#                     q.append(node.right)
-#             if rev:
-#                 curr.reverse()
-#             res.append(curr)
-#             size *= 2
-#         return res
+        # handle edge case
+        if not root: return []
+        # use bfs with deque
+        dq = deque()
+        dq.append(root)
+        res = []
+        dir = True
+        # while depth still has nodes
+        while dq:
+            curr = []
+            new_dq = deque()
+            while dq:
+                # get next node in queue and process
+                node = dq.popleft()
+                curr.append(node.val)
+                if node.left: new_dq.append(node.left)
+                if node.right: new_dq.append(node.right)
+            # reverse current depth every other depth
+            if not dir:
+                curr.reverse()
+            res.append(curr)
+            # iterate variables for next depth
+            dq = new_dq
+            dir = not dir
+        return res
