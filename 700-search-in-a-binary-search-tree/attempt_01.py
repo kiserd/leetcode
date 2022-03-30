f224a668221f94a60fb3c64526583932606655df
kiserd <kiserd@oregonstate.edu>
2022-03-30 07:40:17 -0700

    working through recursion exercises

--- 700-search-in-a-binary-search-tree/attempt_01.py
+++ 700-search-in-a-binary-search-tree/attempt_01.py
@@ -6,3 +6,11 @@
 #         self.right = right
 class Solution:
     def searchBST(self, root, val: int):
+        # handle base case(s)
+        if not root or root.val == val:
+            return root
+        # handle recursive case
+        if val > root.val:
+            return self.searchBST(root.right, val)
+        else:
+            return self.searchBST(root.left, val)
