46bb72870d54391018fc6ac45df993eb3f8cc616
kiserd <kiserd@oregonstate.edu>
2022-03-30 06:36:24 -0700

    reversed linked list

---
+++ 206-reverse-linked-list/attempt_01.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head):
+        # handle edge case
+        if not head:
+            return head
+        # get reference to end of list and head of remainder
+        new_head = head
+        rem = head.next
+        def helper(end, rem):
+            # handle base case
+            if not rem:
+                return end
+            # handle recursive case
+            new_rem = rem.next
+            rem.next = end
+            return helper(rem, new_rem)
+        # turn head into tail
+        head.next = None
+        return helper(new_head, rem)
